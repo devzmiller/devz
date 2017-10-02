c98fd01319d105f833ba53b9d36cc2f6f0f204e0
Devin Miller <devzmiller@gmail.com>
2017-10-01 22:24:56 -0700

    Add poem edit functionality

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -16,9 +16,22 @@ get '/poems/:id' do
   erb :'/poems/show'
 end
 
+get '/poems/:id/edit' do
+  @poem = Poem.find(params[:id])
+  erb :'/poems/edit'
+end
+
 delete '/poems/:id' do
   @poem = Poem.find(params[:id])
   @poem.destroy
+  redirect '/poems'
+end
+
+put '/poems/:id' do
+  @poem = Poem.find(params[:id])
+  @poem.title = params[:title]
+  @poem.body = params[:body]
+  @poem.save
   redirect '/poems'
 end
 
