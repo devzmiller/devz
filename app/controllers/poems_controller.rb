b73264779d31d7524f226b8c9ac1091f509827fa
Devin Miller <devzmiller@gmail.com>
2017-10-01 21:56:11 -0700

    Add poem show page

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -6,3 +6,8 @@ get '/poems' do
   @poems = Poem.all
   erb :'/poems/index'
 end
+
+get '/poems/:id' do
+  @poem = Poem.find(params[:id])
+  erb :'/poems/show'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Poem.destroy_all
 10.times do
   body = ""
   10.times do
-    body += Faker::HarryPotter.quote + "\n"
+    body += Faker::HarryPotter.quote + "<br>"
   end
   Poem.create(title: Faker::Book.title, body: body)
 end
