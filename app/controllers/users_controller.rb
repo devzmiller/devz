eff53fc84e5aac7c939877498348e7a5d8bcccda
Devin Miller <email": null>
2017-10-03 11:34:23 -0700

    Create new user form with error handling and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 get '/users/new' do
-  # erb :'/users/new'
+  erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.valid?
+    @user.save
+    redirect '/sessions/new'
+  else
+    @errors = @user.errors
+    erb :'/users/new'
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,4 +7,20 @@ describe 'UsersController' do
       expect(last_response.status).to eq 200
     end
   end
+
+  describe 'post /users' do
+    it 'returns a 302 +-1 status' do
+      post '/users', {user: {name: "Hamilton Hamstein", email: "ham@ham.com", password: "ham"}}
+      expect(last_response.status).to be_within(1).of(302)
+    end
+    it 'creates a new user' do
+      post '/users', {user: {name: "Hamilton Hamstein", email: "ham@ham.com", password: "ham"}}
+      user = User.find_by_email("ham@ham.com")
+      expect(user).to_not be_nil
+    end
+    it 'if user is invalid, display errors' do
+      post '/users', {user: {name: "Hamilton Hamstein", email: "ham@ham.com"}}
+      expect(last_response.status).to eq 200
+    end
+  end
 end
