0788115ad9e09f4e9b5c86bf3c84490d6bff249f
Devin Miller <email": null>
2017-10-03 16:16:32 -0700

    Add login/new user form css

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,7 @@ describe 'SessionsController' do
   describe 'login form: /sessions/new' do
     it 'returns 200 status' do
       get '/sessions/new'
-      expect(last_response.status).to eq 200
+      expect(last_response).to be_ok
     end
   end
 
@@ -12,14 +12,12 @@ describe 'SessionsController' do
     let!(:user) { User.create!(name: "Hamilton Hamstein", email: "ham@ham.com", password: "ham")}
     it 'returns a 302 +- 1 status' do
       post '/sessions', {email: "ham@ham.com", password: "ham"}
-      expect(last_response.status).to be_within(1).of(302)
+      expect(last_response.redirect?).to be true
     end
-
     it 'redirects to /users/:id' do
       post '/sessions', {email: "ham@ham.com", password: "ham"}
       expect(last_response.location).to end_with("/users/#{user.id}")
     end
-
     it 'if username or password is invalid, displays login page again' do
       post '/sessions', {email: "bob@ham.com", password: "ham"}
       expect(last_response.status).to eq 200
@@ -35,11 +33,6 @@ describe 'SessionsController' do
     it 'redirects to the /poems page' do
       delete "/sessions/#{user.id}", {}, "rack.session" => {user_id: "#{user.id}"}
       expect(last_response.location).to end_with "/poems"
-    end
-    xit 'deletes the session user_id' do
-      delete "/sessions/#{user.id}", {}, "rack.session" => {user_id: "#{user.id}"}
-      p ENV["rack.session"]
-      # expect().to eq []
     end
   end
 end
